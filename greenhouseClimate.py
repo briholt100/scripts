30923d3f24d59661926e20c7ad32970a8e695435
Brian Holt <brian.c.holt@gmail.com>
2015-03-18 16:48:33 -0700

    edit class to take days at init)

--- greenhouseClimate.py
+++ greenhouseClimate.py
@@ -4,37 +4,32 @@ Created on Wed Mar 18 13:38:37 2015
 
 @author: bholt
 
-
-
-
-    def TempCycle (minutes,temp=70,tempPosition=20):
-
-
 """
 
 
 
 class Climate(object):
     
-    def __init__(self,temp,humidity,airFlow,days):
-        self.temp=temp        
+    def __init__(self,tempAvg=68,humidity=.45,airFlow=1,days=1):
+        self.tempAvg=tempAvg        
         self.humidity=humidity
         self.airFlow=airFlow
         self.days=days
                 
-    def TempCycle(self,minutes,tempPosition=20):
+    def TempCycle(self,minutes=10,tempPosition=20):
         import time,random
         import numpy as np
-        print "degrees"+"\t "+str(self.temp-5) + "\t" * 2 + str(self.temp)+ "." * 10+str(self.temp+5)    
-        #temp = 70
-        #tempPosition=20
-        for sec in range(1,minutes+1):
-            randNum=random.uniform(-2,2)    
-            if randNum < 0:
-                tempPosition+= np.floor(randNum)
-            if randNum > 0:
-                tempPosition += np.ceil(randNum)
-            print str(sec)+"|" + " " * tempPosition + "%.2f" % (self.temp)
-            self.temp+=randNum
-            time.sleep(.1)
-            sec+=1
+        print "\n\n  F°" + " " * tempPosition + str(self.tempAvg)  
+        for d in range(1,self.days+1):
+            print "\nA New Day "+ str(d)  +"\n"
+            for sec in range(1,minutes+1):
+                randNum=random.uniform(-2,2)    
+                if randNum < 0:
+                    tempPosition+= np.floor(randNum)
+                if randNum > 0:
+                    tempPosition += np.ceil(randNum)
+                print str(sec)+"|" + " " * tempPosition + "%.2f" % (self.tempAvg)
+                self.tempAvg+=randNum
+                time.sleep(.1)
+                sec+=1
+            d+=1

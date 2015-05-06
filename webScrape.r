65028a6fc16e1b24f6968cec42b2c7bab2fecd3c
Brian Holt <brian.c.holt@gmail.com>
2015-05-05 20:54:37 -0700

    finished the convert from factor

--- webScrape.r
+++ webScrape.r
@@ -1,4 +1,4 @@
-install.packages("XML")
+library(dplyr)
 library(XML)
 theurl='http://americasmarkets.usatoday.com/2015/04/30/surprise-women-trump-men-on-ceo-pay/'
 
@@ -10,5 +10,17 @@ tables[[which.max(n.rows)]]
 names<-as.matrix(tables[[1]][1,])
 df<-data.frame(tables[[1]])
 colnames(df)<-names
+df<-df[c(-1),]
+rownames(df)<-NULL
+a=NULL
+for (i in 1:length(df)){
+  a<-cbind(a,as.character(df[,i]))
+}
+colnames(a)<-colnames(df)  
+df<-(a)
+df<-gsub("\\$|\\*","",df,ignore.case=T)
+df[,4]<-gsub(" ","",df[,4],ignore.case=T)
+df<-as.data.frame(df)
+df[,4]<-as.numeric(as.character(df[,4]))
 
-class(df[,4])
+df[df[,4]>30 | df[,4]<21,]

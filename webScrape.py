ec272757446492a19b723e27daacd16d113a5c51
Brian Holt <brian.c.holt@gmail.com>
2015-05-27 21:41:12 -0700

    adding examples of selenium select

--- webScrape.py
+++ webScrape.py
@@ -139,3 +139,44 @@ dir(element)
 
 
 
+
+
+
+#Try this#http://stackoverflow.com/questions/12164205/selenium-selecting-a-dropdown-option-with-for-loop-from-dictionary
+#####using selenium 
+#ScriptName : Login.py
+#---------------------
+from selenium import webdriver
+
+#Following are optional required
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+
+baseurl = "http://www.mywebsite.com/login.php"
+username = "admin"
+password = "admin"
+
+xpaths = { 'usernameTxtBox' : "//input[@name='username']",
+           'passwordTxtBox' : "//input[@name='password']",
+           'submitButton' :   "//input[@name='login']"
+         }
+
+mydriver = webdriver.Firefox()
+mydriver.get(baseurl)
+mydriver.maximize_window()
+
+#Clear Username TextBox if already allowed "Remember Me" 
+mydriver.find_element_by_xpath(xpaths['usernameTxtBox']).clear()
+
+#Write Username in Username TextBox
+mydriver.find_element_by_xpath(xpaths['usernameTxtBox']).send_keys(username)
+
+#Clear Password TextBox if already allowed "Remember Me" 
+mydriver.find_element_by_xpath(xpaths['passwordTxtBox']).clear()
+
+#Write Password in password TextBox
+mydriver.find_element_by_xpath(xpaths['passwordTxtBox']).send_keys(password)
+
+#Click Login button
+mydriver.find_element_by_xpath(xpaths['submitButton']).click()

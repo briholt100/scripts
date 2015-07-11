5e9bac0086d902ce167e3a4ab1476b9f01e2d2e0
briholt100 <brian.c.holt@gmail.com>
2015-07-10 21:05:22 -0700

    changed directory to quiz 5

--- xmlParse.r
+++ xmlParse.r
@@ -1,12 +1,12 @@
 library(XML)
-#setwd("I:\\work\\Lifespan\\ch.2.1.med WebCT 07012015 190950\\QIZ_5553724_M\\data")
+
 #windater
-#setwd("I:\\work\\Lifespan\\ch4 WebCTtest\\QIZ_5553724_M\\data")
+#setwd("I:\\work\\Lifespan\\quizzes\\ch5\\QIZ_5553724_M\\data")
 #dater
 #setwd("/media/brian/dater_bridge2/work/Lifespan/ch4 WebCTtest/QIZ_5553724_M/data")
 dir()
 
-doc<-xmlTreeParse("./ch4.xml" ,useInternalNodes=F)
+doc<-xmlTreeParse("./ch5.xml" ,useInternalNodes=F)
 
 root<-xmlRoot(doc)
 xmlName(root)

f06696f16d74fa2649446a960726fc146d070b52
Brian Holt <brian.c.holt@gmail.com>
2015-09-15 11:42:05 -0700

    parses fine into tuple:

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -5,12 +5,25 @@ Created on Mon Sep 14 16:48:52 2015
 @author: brian
 """
 
-""" open excel file, read each line, parse the address, add to dictinoary? and to line"""
+""" 
+#open excel file, 
+#read each line change to title --upper first letter
+#parse the address
+
+return the updated address
+
+
+#bonus:
+strip unnecessary commas
+add appropriate punctuation
+validate with usps?
+
+"""
 
 import usaddress
 
 
-directions = ['n','s','w','e','ne','nw','se','sw','North','South','West','East','Northwest','Southwest', 'Northeast','Southeast']
+#directions = ['n','s','w','e','ne','nw','se','sw','North','South','West','East','Northwest','Southwest', 'Northeast','Southeast']
 
 
 
@@ -18,23 +31,17 @@ with open("../data/addresses.csv") as file:
     while True:
         line = file.readline()
         address = line.title()
-        print "\n"+address
+        print "\n"+address+"\n"
         address = usaddress.parse(address)
-        for i in address:
-            print i
+        for i, (v,d) in enumerate(address):
+            if d == "StateName":
+                #v=v.upper()                
+                print i, d, v
+                print address[i]
         if not line: break
     file.close()
     
+    
+"""need to either update the tuple (I don't think that's possible) or to make a new dictionary, but return that dictionary"""       
+    
 addr='16614 Stone Ave N, shoreline, wa, 98133-5425'
-
-#
-
-up= (addr.upper())
-
-
-home=usaddress.parse(addr)
-
-print home
-
-
-usaddress.parse(up)

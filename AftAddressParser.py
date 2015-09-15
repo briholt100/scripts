e8daa2cc5e4f27007d4a1fab9b7932913cbc1baa
Brian Holt <brian.c.holt@gmail.com>
2015-09-15 15:06:14 -0700

    parsing works, needs to make a dictionary or list

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -20,14 +20,12 @@ validate with usps?
 
 """
 
-import usaddress
+import usaddress, os
 
-
+os.chdir("/home/brian/Projects/scripts")
 #directions = ['n','s','w','e','ne','nw','se','sw','North','South','West','East','Northwest','Southwest', 'Northeast','Southeast']
 
-
-
-with open("../data/addresses.csv") as file:
+"""with open("../data/addresses.csv") as file:
     while True:
         line = file.readline()
         address = line.title()
@@ -35,13 +33,31 @@ with open("../data/addresses.csv") as file:
         address = usaddress.parse(address)
         for i, (v,d) in enumerate(address):
             if d == "StateName":
-                #v=v.upper()                
+                v=v.upper()                
                 print i, d, v
                 print address[i]
         if not line: break
     file.close()
-    
+"""
+
+with open("../data/addresses.csv") as file:
+    while True:
+        line = file.readline()
+        print line
+        address = line#.title()
+        #print "\n"+address+"\n"
+        address = usaddress.tag(address)
+        for key, value in address[0].items():
+            print  value.title()
+        if not line: break
+    file.close()
+
+
     
 """need to either update the tuple (I don't think that's possible) or to make a new dictionary, but return that dictionary"""       
     
 addr='16614 Stone Ave N, shoreline, wa, 98133-5425'
+
+    
+    
+    

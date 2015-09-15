a167da7e009199f4b23b02411498346fdeb429da
Brian Holt <brian.c.holt@gmail.com>
2015-09-15 09:54:36 -0700

    repaired break statement

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -8,8 +8,6 @@ Created on Mon Sep 14 16:48:52 2015
 """ open excel file, read each line, parse the address, add to dictinoary? and to line"""
 
 import usaddress
-from collections import defaultdict
-
 
 
 directions = ['n','s','w','e','ne','nw','se','sw','North','South','West','East','Northwest','Southwest', 'Northeast','Southeast']
@@ -22,7 +20,9 @@ with open("../data/addresses.csv") as file:
         address = line.title()
         print "\n"+address
         address = usaddress.parse(address)
-        
+        for i in address:
+            print i
+        if not line: break
     file.close()
     
 addr='16614 Stone Ave N, shoreline, wa, 98133-5425'
@@ -33,10 +33,6 @@ up= (addr.upper())
 
 
 home=usaddress.parse(addr)
-
-for i,v in home:
-    print i,v
-
 
 print home
 

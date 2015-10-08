cd73538da2553d3c6a9e44b1e138c56f6b5abe7c
Brian Holt <brian.c.holt@gmail.com>
2015-10-07 23:34:37 -0700

    dictionary success..need an ordered dic?

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -3,19 +3,9 @@
 Created on Mon Sep 14 16:48:52 2015
 
 @author: brian
-"""
-
-""" 
-#open excel file, 
-#read each line change to title --upper first letter
-#parse the address
-
-return the updated address
 
 
-#bonus:
-strip unnecessary commas
-add appropriate punctuation
+make addressDic an ordered dict
 validate with usps?
 
 """
@@ -25,55 +15,30 @@ os.getcwd()
 os.chdir("/home/brian/Projects/scripts")
 #dataPath="/home/brian/Projects/data")
 #for campus
-os.chdir("I:\My Data Sources\Scripts")
+#os.chdir("I:\My Data Sources\Scripts")
 #dataPath=os.chdir("/home/brian/Projects/scripts")
 #directions = ['n','s','w','e','ne','nw','se','sw','North','South','West','East','Northwest','Southwest', 'Northeast','Southeast']
-
-"""with open("../data/addresses.csv") as file:
-       while True:
-        line = file.readline()
-        address = line.title()
-        print "\n"+address+"\n"
-        address = usaddress.parse(address)
-        for i, (v,d) in enumerate(address):
-            if d == "StateName":
-                v=v.upper()                
-                print i, d, v
-                print address[i]
-        if not line: break
-    file.close()
-"""
-
+#addr='16614 Stone Ave N, shoreline, wa, 98133-5425'
 with open("../data/addresses.csv") as file:
     addressDic={} 
-    addressList=[]
     while True:
         line = file.readline()
         #print line
         address = line#.title()
         #print "\n"+address+"\n"
         address = usaddress.tag(address)
-        for key, value in address[0].items():
-           print  value.title()
-            #addressDic.update({key:value})
-            #addressList.append(value)
         for i,(key,value) in enumerate(address[0].items()):
-            #addressDic.update({value[0]:value[1:]})
-            print i, key,value
-            #print address[0].get('Recipent')
-        name=address[0].get('Recipient')
+            print key, value 
+            addressDic.update({address[0].values()[0].title():{k:v.title() for k,v in address[0].items()}}) 
+            name=address[0].values()[0]
         print "the recipient's name is "+str(name)
-        address[0].viewkeys()
+        print "\n"
         if not line: break
     file.close()
 
-addressDic
+addressDic.items()
 
 
-    
-"""need to either update the tuple (I don't think that's possible) or to make a new dictionary, but return that dictionary"""       
-    
-addr='16614 Stone Ave N, shoreline, wa, 98133-5425'
 
     
     

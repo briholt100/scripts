caab7ed61e5419d43b2c56f417b4ae391cc5a518
Brian Holt <brian.c.holt@gmail.com>
2015-10-10 13:10:29 -0700

    creates an new ordered dict; needs to flag none recipient fields

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -6,9 +6,13 @@ Created on Mon Sep 14 16:48:52 2015
 
 to do:
 verify that there is a recipient name, if not, create a flag of some kind
-make addressDic an ordered dict
 produce a new file with formated address?  separate values with /n?
 validate with usps?
+
+I could have used a regular dictionary, but chose an ordered
+and trust the usaddress module's choice to use an ordered dic; also
+makes it easier to recreate a useable address.
+
 
 """
 scriptLoc = "/home/brian/Projects/scripts"
@@ -16,33 +20,33 @@ import usaddress, os, collections
 os.getcwd()
 os.chdir(scriptLoc)
 with open("../data/addresses.csv") as file:  #opens file to readeach line
-    addressDic={} #creates dictionary to hold parased data
-    addressOrdered=collections.OrderedDict() #creates ordered dictionary to hold parased data
-    while True: #something about how 'if line then true' else false, closes this while loop
+    addressDic={} # For parased data
+    addressOrdered={} # Ordered dict for parased data
+    #something like 'if line then true' else false, closes this while loop    
+    while True: 
         line = file.readline()
-        address = line  #could just keep line
+        address = line 
         #print "\n"+address+"\n"
-        address = usaddress.tag(address) # this will parse based on usaddress algorithm
-        
-        for i,(key,value) in enumerate(address[0].items()): #iterates over each item pair in the ordered dict
+        # This will parse based on usaddress algorithm
+        address = usaddress.tag(address) 
+        # Iterates over each item pair in the ordered dict
+        for i,(key,value) in enumerate(address[0].items()):
             print key, value 
-
-            addressOrdered.update({address[0].values()[0].title():collections.OrderedDict([(key,value) for k,v in address[0].items()])})  #close...the nested dict in the values are not complete.            
-#######below works for making a new dict.        
-            addressDic.update({address[0].values()[0].title():{k:v.title() for k,v in address[0].items()}}) 
-            #previous line updates dictionary, the key being the first value of address[0] 
-            #while the value is the remaining prased values from the address ordered dictionary
-            # .title() chnages the values to be punctuated
-            # the last portion is a dict comprehension to populate addressDic update
-            name=address[0].values()[0]  #just a test to pull the first ordered value from the address line
+            # Updates dictionary, key being first value of address[0] 
+            # while the 'value' is the remaining from the ordered dictionary
+            # .title() changes the values to be punctuated
+            # the last portion is dict comprehension to populate addressDic
+            addressOrdered.update({address[0].values()[0].title():
+                collections.OrderedDict([(k,v) for
+                            k,v in address[0].items()])})  
+            #makes an unordered, standard dictionary
+            addressDic.update({address[0].values()[0].title():{k:v.title() for
+                               k,v in address[0].items()}}) 
+            name=address[0].values()[0]  # Just a test to pull the first value
         print "the recipient's name is "+str(name)
         print "\n"
         if not line: break
-    file.close()  #must close the file!
+    file.close()  # Must close the file!
 
 print addressDic.items()[0:3]
-
-
-test=collections.OrderedDict([(k,v) for k,v in addressDic.items()])  #works as expected
-print test
-print test.update(addressOrdered) #works as expected
+print addressOrdered.items()[0:3]

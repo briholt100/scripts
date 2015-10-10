c6226c174965cf6c0debb71e5e9698d3ed5d4df4
Brian Holt <brian.c.holt@gmail.com>
2015-10-10 12:23:46 -0700

    working on ordered dic, nested dict in values incomplete

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -12,18 +12,23 @@ validate with usps?
 
 """
 scriptLoc = "/home/brian/Projects/scripts"
-import usaddress, os
+import usaddress, os, collections
 os.getcwd()
 os.chdir(scriptLoc)
 with open("../data/addresses.csv") as file:  #opens file to readeach line
     addressDic={} #creates dictionary to hold parased data
+    addressOrdered=collections.OrderedDict() #creates ordered dictionary to hold parased data
     while True: #something about how 'if line then true' else false, closes this while loop
         line = file.readline()
         address = line  #could just keep line
         #print "\n"+address+"\n"
         address = usaddress.tag(address) # this will parse based on usaddress algorithm
+        
         for i,(key,value) in enumerate(address[0].items()): #iterates over each item pair in the ordered dict
             print key, value 
+
+            addressOrdered.update({address[0].values()[0].title():collections.OrderedDict([(key,value) for k,v in address[0].items()])})  #close...the nested dict in the values are not complete.            
+#######below works for making a new dict.        
             addressDic.update({address[0].values()[0].title():{k:v.title() for k,v in address[0].items()}}) 
             #previous line updates dictionary, the key being the first value of address[0] 
             #while the value is the remaining prased values from the address ordered dictionary
@@ -36,3 +41,8 @@ with open("../data/addresses.csv") as file:  #opens file to readeach line
     file.close()  #must close the file!
 
 print addressDic.items()[0:3]
+
+
+test=collections.OrderedDict([(k,v) for k,v in addressDic.items()])  #works as expected
+print test
+print test.update(addressOrdered) #works as expected

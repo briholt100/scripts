193af1db9bd75defcca0b600613b92b9ffebb255
Brian Holt <brian.c.holt@gmail.com>
2015-10-10 22:55:22 -0700

    makes appropriate lists, dict; needs to write new file formated nicely

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -5,7 +5,8 @@ Created on Mon Sep 14 16:48:52 2015
 @author: brian
 
 to do:
-verify that there is a recipient name, if not, create a flag of some kind
+check that will accept multi field csv..YES
+verify that there is a recipient name, if not, create Missing list....YES
 produce a new file with formated address?  separate values with /n?
 validate with usps?
 
@@ -22,31 +23,39 @@ os.chdir(scriptLoc)
 with open("../data/addresses.csv") as file:  #opens file to readeach line
     addressDic={} # For parased data
     addressOrdered={} # Ordered dict for parased data
-    #something like 'if line then true' else false, closes this while loop    
+    missRecipList=[]
+        #something like 'if line then true' else false, closes this while loop    
     while True: 
         line = file.readline()
         address = line 
-        #print "\n"+address+"\n"
         # This will parse based on usaddress algorithm
         address = usaddress.tag(address) 
+        # looks for addresses without a recipient, makes list of them        
+        for k,v in address[0].items(): # add [0:1] after .items() for whole record
+            if not address[0].has_key('Recipient'):
+                missRecipList.append(v)
         # Iterates over each item pair in the ordered dict
         for i,(key,value) in enumerate(address[0].items()):
-            print key, value 
+            #            
+            # Print key, value 
             # Updates dictionary, key being first value of address[0] 
-            # while the 'value' is the remaining from the ordered dictionary
+            # While the 'value' is the remaining from the ordered dictionary
             # .title() changes the values to be punctuated
-            # the last portion is dict comprehension to populate addressDic
+            # The last portion is dict comprehension to populate addressDic
+            #            
             addressOrdered.update({address[0].values()[0].title():
-                collections.OrderedDict([(k,v) for
-                            k,v in address[0].items()])})  
+                collections.OrderedDict([(k,v) 
+                    for k,v in address[0].items()])})  
             #makes an unordered, standard dictionary
             addressDic.update({address[0].values()[0].title():{k:v.title() for
                                k,v in address[0].items()}}) 
-            name=address[0].values()[0]  # Just a test to pull the first value
-        print "the recipient's name is "+str(name)
-        print "\n"
+
         if not line: break
     file.close()  # Must close the file!
 
-print addressDic.items()[0:3]
-print addressOrdered.items()[0:3]
+
+
+print "\n\nmissing a recipient includes the following:\n\n " + str(missRecipList)
+
+"""print addressDic.items()[0:3]
+print addressOrdered.items()[0:3]"""

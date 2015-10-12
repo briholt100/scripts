6ded4176852568500a81bdfdcdd83d2b0f6418fc
Brian Holt <brian.c.holt@gmail.com>
2015-10-11 22:50:47 -0700

    make missing list a dict; finish formatting

--- AftAddressParser.py
+++ AftAddressParser.py
@@ -5,8 +5,8 @@ Created on Mon Sep 14 16:48:52 2015
 @author: brian
 
 to do:
-check that will accept multi field csv..YES
-verify that there is a recipient name, if not, create Missing list....YES
+
+verify that there is a recipient name, if not, create Missing list, should be dict?
 produce a new file with formated address?  separate values with /n?
 validate with usps?
 
@@ -20,7 +20,8 @@ scriptLoc = "/home/brian/Projects/scripts"
 import usaddress, os, collections
 os.getcwd()
 os.chdir(scriptLoc)
-with open("../data/addresses.csv") as file:  #opens file to readeach line
+
+with open("../data/addresses.csv") as file:
     addressDic={} # For parased data
     addressOrdered={} # Ordered dict for parased data
     missRecipList=[]
@@ -31,7 +32,7 @@ with open("../data/addresses.csv") as file:  #opens file to readeach line
         # This will parse based on usaddress algorithm
         address = usaddress.tag(address) 
         # looks for addresses without a recipient, makes list of them        
-        for k,v in address[0].items(): # add [0:1] after .items() for whole record
+        for k,v in address[0].items()[0:1]: # add [0:1] after .items() for whole record
             if not address[0].has_key('Recipient'):
                 missRecipList.append(v)
         # Iterates over each item pair in the ordered dict
@@ -44,19 +45,44 @@ with open("../data/addresses.csv") as file:  #opens file to readeach line
             # The last portion is dict comprehension to populate addressDic
             #            
             addressOrdered.update({address[0].values()[0].title():
-                collections.OrderedDict([(k,v) 
+                collections.OrderedDict([(k,v.title()) 
                     for k,v in address[0].items()])})  
+            
             #makes an unordered, standard dictionary
             addressDic.update({address[0].values()[0].title():{k:v.title() for
                                k,v in address[0].items()}}) 
-
         if not line: break
     file.close()  # Must close the file!
+    
+with open('../data/formattAddress.csv', "w") as outfile:
+    for k,v in addressOrdered.items():
+        print "\n"+ k + "\n"
+        for i,j in v.items():
+            print i + " " + v[i]
+            #outfile.write(j + ",") 
+    outfile.close()
+
+"""
+pseudo code for creatinga  file
+
+print recipient
+print number + street + posttype + direction + and so on until reach "PlaceName"
+make newline
+print PlaceName comma New line
+print StateName
+print ZipCode
+
+
+print addressOrdered['Recipient']
+
+"""
 
 
 
-print "\n\nEntries that are missing a recipient\
-includes the following: " + str(missRecipList[0])  #note the call of just 1 item
+
+
+print "\n\nEntries that are missing a recipient\n \
+ includes the following: \n" + str(missRecipList)  #[0] calls first item
 
 """print addressDic.items()[0:3]
 print addressOrdered.items()[0:3]"""

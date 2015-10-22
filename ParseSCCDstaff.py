2f3ee682941cb48c8819a3e4ec593f9bf8b05e0b
briholt100 <brian.c.holt@gmail.com>
2015-10-22 13:27:47 -0700

    rows need merging

--- ParseSCCDstaff.py
+++ ParseSCCDstaff.py
@@ -6,13 +6,14 @@ Created on Wed Oct 21 21:58:49 2015
 """
 
 url='https://inside.seattlecolleges.edu/directory/content/results.asp'
-import requests, collections, BeautifulSoup, re, bs4
+import re, requests, collections, BeautifulSoup,  bs4
 from requests.auth import HTTPDigestAuth, HTTPBasicAuth
 
-user=
-passwrd=
+#user=
+#passwrd=
 r=requests.get(url, auth=(user,passwrd))
 r.status_code
+i=0
 for i,l in enumerate(r.iter_lines()):
     while i < 500:
         print i, l
@@ -25,14 +26,33 @@ print text.prettify()
 # Below is openning a saved CSV file
 #==============================================================================
 pattern=r'Faculty'   # pattern selects what is between ""
+
 i=0
-with open('/home/brian/Projects/data/SCCDstaff.csv') as infile:
+#==============================================================================
+# Campus
+#==============================================================================
+with open('I://My Data Sources//Data//SCCDstaff.csv') as infile:
+#==============================================================================
+# Windater
+#==============================================================================
+#with open('/home/brian/Projects/data/SCCDstaff.csv') as infile:
     while True:
-        line = infile.readline()
+        line = infile.readlines()
         if not line: break        
-        fac = re.findall(pattern, line)   
-        if fac:
-            print line
-        elif:
+        if str(line).strip('\n'):
+            for i,x in enumerate(line):
+                print '|'.join(['Contact...%d' % i] +x.strip('\n').split('\n'))            
+            #print line
+        #elif:
         i+=1
     infile.close()
+    
+'|'.join(['line%d' % i] +   x .strip('\n').split('\n')
+    ['|'.join(['line%d' % i] + x.strip('\n').split('\n')) 
+         for i, x in enumerate(re.split('line[0-9]+', l)) 
+             if x.strip('\n')]
+                 
+                 
+                 #fac = re.findall(pattern, line) #this is problematic because it ignores other lines of importance  
+        #if fac:
+        

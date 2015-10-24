b3930cf68cfa50f8ce8de68662f89f936ec5b029
Brian Holt <brian.c.holt@gmail.com>
2015-10-23 19:49:15 -0700

    adding general scrap

---
+++ scrap.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 23 16:12:05 2015
+
+@author: brian
+"""
+
+temp=['hello there\t\tQ','goodbye\n','comeback!\n']
+temp
+len(temp)
+
+
+
+for item in (temp):    
+    item=re.sub('\t|\n','fuck fuck fuck',str(item))
+temp
+
+
+
+
+a = ['a', 'b', 'c']
+res = "".join(a)
+res

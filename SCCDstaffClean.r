161d6a5435dfe797203cd92008dd74c33dfbb8a8
Brian Holt <brian.c.holt@gmail.com>
2015-10-24 22:24:08 -0700

    df_fac cleaned and emailed

--- SCCDstaffClean.r
+++ SCCDstaffClean.r
@@ -2,9 +2,12 @@ library(tidyr)
 df<-read.csv('/home/brian/Projects/data/outfile.csv',sep='\t',strip.white=T)
 df$mailstop<-gsub(" ","",df$mailstop)
 df$name<-gsub(", II"," II",df$name)
-split<-strsplit(as.character(df$name),", ")
+df[duplicated(df$name),]
+"split<-strsplit(as.character(df$name),", ")
 split
-split[table(lapply(split,length)>2)]
+table(lapply(split,length)==2)"
+df<-separate(df, name, c("LastName", "FirstName"), ", ",remove=T)
+
 
 
 
@@ -14,16 +17,12 @@ pt<-grep('Faculty|ibrar|ounsel',df$emp_status,value=F)
 
 df_fac<-df[pt,]
 df_fac<-droplevels(df_fac)
-sort(table((df_fac$mailstop))>100)
-length(df_fac$emp_status)
-grep('ibrar|ounse|aculty',df_fac$emp_status,value=T)
 
-tail(df_fac)
 dean<-grep('ean',df_fac$emp_status,value=F)
 df_fac<-df_fac[-dean,]
 
 df_fac[df_fac$campus=='North Campus',]
-extract(df, name, c("LastName", "FirstName"), ", ")
+head(df_fac[df_fac$campus=='North Campus',])
 
-
+write.table(df_fac,"/home/brian/Projects/data/fac_contacts.csv",row.names=F,sep="\t")
 

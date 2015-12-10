da33baad1043c3986b4275bdfd67bd36b244410f
briholt100 <brian.c.holt@gmail.com>
2015-12-10 15:58:12 -0800

    playnig with ggplot jitter, stat-_sumary

--- anova.r
+++ anova.r
@@ -3,6 +3,7 @@ library('ggplot2')
 library('dplyr')
 library('pastecs')
 library('nlme')
+library('Hmisc')
 ##the 'na' below is actually a 4
 bogart<-data.frame(rbind(c('subj1',2.0,4,5.0,9.0),c('subj2',3,6,7,10),c('subj3',4,5,6,8),c('subj4',4,6,7,10),c('subj5',5,5,7,9)))
 class(bogart)
@@ -28,9 +29,21 @@ anova(base,model)
 
 summary(model)
 
+
+
+m.bogart
+#lci<-by(m.bogart$score,m.bogart$subject,FUN=sd)
+#uci<-
+p<-ggplot(m.bogart,aes(x=treatment,y=score,group=subject,color=subject))
+p+stat_summary(fun.y=mean,geom='line',position = position_jitter(w = 0.3, h = 0.3)) +ylim(0, 12)
+p+geom_line(position = position_jitter(w = 0.3, h = 0.3))+stat_summary(fun.data='mean_cl_normal',geom='errorbar',mult=5,color='blue')+ylim(0, 12)
+p+geom_point(position = position_jitter(w = 0.3, h = 0.3))
+
+
+
 "m.bogart<-m.bogart%>%mutate(mean=mean(score,na.rm=T),devi= score - mean(score,na.rm=T),sq_dev = devi^2,variance=var(score,na.rm=T))
 m.bogart%>%group_by(subject)%>%summarise(n=n_distinct(treatment))
-mean(m.bogart[,3],na.rm=T) # this should be grand mean
+ mean(m.bogart[,3],na.rm=T) # this should be grand mean
 sum( m.bogart[, 6]) # this should be grand sum of squres
 
 by(m.bogart$score,m.bogart$subject,stat.desc)

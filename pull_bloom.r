2a583a22735956c9f77e7f6141fc81853ed8e7f2
briholt100 <brian.c.holt@gmail.com>
2016-01-25 10:34:12 -0800

    added comments, cleaned up some varialbe names

--- pull_bloom.r
+++ pull_bloom.r
@@ -18,20 +18,28 @@ links<-cbind(xpathSApply(bloom,searchTerms,xmlValue), # this pulls the <a> names
 
 nrow(links)
 head(links)
+
+# The following loops through 'links', using the URL to read the html 
+# and pull the text out of the <pre> tag
+# then adds it to 'mylist', a nested list
 mylist<-list()
 for (i in 1:nrow(links)){
   text<-links[i,2] %>% url %>% read_html() %>% html_nodes (xpath= '//pre')%>%html_text(trim=F) #this pulls out the actual pre text
-  mylist[[i]]<-text
-  mylist[[i]][2]<-links[i,1]
+  mylist[[i]]<-text  # This puts the text into each element
+  mylist[[i]][2]<-links[i,1] # Puts the agency information into the 2nd element of the list
 }
 
-df<-list()
+
+# The following loops through each element of 'mylist', adds a 4 column dataframe to newly created 'df_list'
+df_list<-list()
 for (i in 1:length(mylist)){
-  df[[i]]<-cbind(mylist[[i]][2],read.fwf(textConnection(mylist[[i]][1]),
+  df_list[[i]]<-cbind(mylist[[i]][2],read.fwf(textConnection(mylist[[i]][1]),
                                          widths=c(32,32,81),
                                          skip=3,
                                          col.names=c('Employee','Job_title','Salary')
   ))
 
 }
-final_df<-do.call("rbind",df)
+final_df<-do.call("rbind",df_list)  # this converts df_list into a dataframe.
+colnames(final_df)<-c('Institution','Employee','Job_title','Salary')
+

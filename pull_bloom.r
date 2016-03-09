cce18db7b85fc49c7d150a44a819930acb8b2ebf
Brian Holt <brian.c.holt@gmail.com>
2016-03-08 23:22:21 -0800

    2009 works, 2011 has extra spaces in employee

--- pull_bloom.r
+++ pull_bloom.r
@@ -38,66 +38,6 @@ for (i in 1:length(page_list)){
 links<-do.call('rbind',links)
 str(links)
 
-#######PROBLEM.  2001-2009 HAS ODD 'FWF', or other odd column formats.  Yuck
-# Several pages have different lines to 'skip'.  fuck off
-
-# go here https://www.youtube.com/watch?feature=player_detailpage&v=q8SzNKib5-4#t=945
-
-#2011...done....2011 has fwf data, name, title, salary
-#2009...done....  6 columns: name, title,  ET-PU, MP, %FT, Salary
-
-############
-########
-###For 2009:
-
-recursive_replace<-function(text=text){
-  text<-gsub('([[:alpha:]]) {2}([[:alpha:]])','\1 \2',text)
-    while (grepl(' {2}',text)){
-    text<-gsub(' {2}','\t',tex)t
-  }
-  while (grepl('\t\t',text)){
-    text<-gsub('\t\t','\t',text)
-    print (grepl('\t\t',text))
-  }
-  return(text)
-}
-
-df_list<-list()
-for (i in 1:length(mylist)){
-  text<-as.character(mylist[[i]][1])
-  text<-gsub('ET-PU  MP  %FT','ET-PU     MP     %FT',text)
-  #if school is UW, then do X, then skip recurisive, else, recursive
-  text<-recursive_replace(text=text)
-  df_list[[i]]<-cbind(mylist[[i]][2],read.delim(textConnection(text),
-                                                header=F,
-                                                strip.white=T,
-                                                skip=2,
-                                                stringsAsFactors=F)
-  )
-  if(length(df_list[[i]])>7){
-    ifelse(sum(is.na(df_list[[i]][,8]))!=nrow(df_list[[i]]),
-           print("error in read.delim; data in extra column"),
-           df_list[[i]]<-df_list[[i]][,-8])
-  }
-}
-
-final_df_2009<-do.call("rbind",df_list)  # this converts df_list into a dataframe.
-colnames(final_df_2009)<-c('Institution','Employee','Job_title','et','mp','percent_ft','Salary')
-final_df_2009<-final_df_2009[(is.na(final_df_2009$Salary))==F,]
-final_df_2009<-final_df_2009[,c(1:3,7,4:6)]
-
-#2001 has tab deliminted data, name, title, salary, but many have 2 column sets!!!!! fuck off lbloom
-#2003 has tab deliminted data, name, title, salary; also astricks first letter first name
-#2005 has mulit-tab deliminted data, name, title, salary
-#2007 has fwf data, but 6 columns: name, title,  ET-PU, MP, %FT, Salary
-
-
-#  Let's start with 2009 and move back, go back to 2001
-links<-links[ grep("2009",links),]
-links<-links[-c(1:3),]
-links<-links[c(1:3),]
-
-
 # The following loops through 'links', using the URL to read the html
 # and pull the text out of the <pre> tag
 # then adds it to 'mylist', a nested list
@@ -118,24 +58,84 @@ mylist<-make_list(links,'2009')
 str(mylist)
 
 
-##For 2011:  Note that Z is added to all last names
-wid<-vector("list",length(mylist))
-for(i in 1:length(mylist)){
-  text<-as.character(mylist[[i]][1])
-  title_start<-gregexpr('  [A-Z]',substr(text,1,200))
-  #name_width<-gregexpr('^+',test)  OY, THIS ISN'T EVEN NEEDED
-  sal_start<-gregexpr(' [0-9]',substr(text,1,200))
-  wid[[i]]<-c(title_start[[1]][1]-3,sal_start[[1]][1]-title_start[[1]][1]+5,40)
-  ###Notice that sal_width might need to pick the 2nd element [[1]][2]
+#######PROBLEM.  2001-2009 HAS ODD 'FWF', or other odd column formats.  Yuck
+# Several pages have different lines to 'skip'.  fuck off
+
+# go here https://www.youtube.com/watch?feature=player_detailpage&v=q8SzNKib5-4#t=945
+
+#2001 has tab deliminted data, name, title, salary, but many have 2 column sets!!!!! fuck off lbloom
+#2003 has tab deliminted data, name, title, salary; also astricks first letter first name
+#2005 has mulit-tab deliminted data, name, title, salary
+#2007 has fwf data, but 6 columns: name, title,  ET-PU, MP, %FT, Salary
+
+#2011 almost works--needs to fix employee extra spaces; fwf data, name, title, salary  but et-pu, mp,%ft are added as NA to rbind with others
+##after running code for 2011 and making data frame, try:
+#table(final_df_2011[grep(' {2,}',final_df_2011$Employee,value=F),1])
+
+
+
+#2009 works; columsn resorted to "institution, employee, title, salary, et-pu, mp,%ft"
+
+############
+########
+###For 2009:
+
+recursive_replace<-function(text=text){
+  text<-gsub('([[:alpha:]]) {2}([[:alpha:]])','\1 \2',text)
+  while (grepl(' {2}',text)){
+    text<-gsub(' {2}','\t',text)
+  }
+  while (grepl('\t\t',text)){
+    text<-gsub('\t\t','\t',text)
+    print (grepl('\t\t',text))
+  }
+  return(text)
 }
+
 df_list<-list()
-for(i in 1:length(mylist)){
+for (i in 1:length(mylist)){
   text<-as.character(mylist[[i]][1])
-  df_list[[i]]<-cbind(mylist[[i]][2],read.fwf(textConnection(text),widths=wid[[i]],
+  text<-gsub('ET-PU  MP  %FT','ET-PU     MP     %FT',text)
+  text<-recursive_replace(text)
+  df_list[[i]]<-cbind(mylist[[i]][2],read.delim(textConnection(text),
                                                 header=F,
                                                 strip.white=T,
                                                 skip=2,
                                                 stringsAsFactors=F)
+  )
+
+  if(length(df_list[[i]])>7){
+    ifelse(sum(is.na(df_list[[i]][,8]))!=nrow(df_list[[i]]),
+           print("error in read.delim; data in extra column"),
+           df_list[[i]]<-df_list[[i]][,-8])
+  }
+}
+
+final_df_2009<-do.call("rbind",df_list)  # this converts df_list into a dataframe.
+colnames(final_df_2009)<-c('Institution','Employee','Job_title','et','mp','percent_ft','Salary')
+final_df_2009<-final_df_2009[(is.na(final_df_2009$Salary))==F,]
+final_df_2009<-final_df_2009[,c(1:3,7,4:6)]
+
+##For 2011:  Note that Z is added to all last names
+wid<-vector("list",length(mylist))
+for(i in 1:length(mylist)){
+  text<-as.character(mylist[[i]][1])
+  title_start<-gregexpr(' Job Title',text)
+  sal_start<-gregexpr('2010 Gross',text)
+  sal_end<-gregexpr('2010 Gross',text)
+  wid[[i]]<-c(title_start[[1]][1]-2,sal_start[[1]][1]+3-title_start[[1]][1]-3,60)
+  ###Notice that sal_width might need to pick the 2nd element [[1]][2]
+}
+wid[[1]]
+
+df_list<-list()
+for(i in 1:length(mylist)){
+  text<-as.character(mylist[[i]][1])
+  df_list[[i]]<-cbind(mylist[[i]][2],read.fwf(textConnection(text),widths=wid[[i]],
+                                              header=F,
+                                              strip.white=T,
+                                              skip=2,
+                                              stringsAsFactors=F)
   )
 }
 
@@ -149,7 +149,10 @@ final_df_2011$mp<-NA
 final_df_2011$percent_ft<-NA
 head(final_df_2011)
 
+
+
+
 df<-rbind(final_df_2009,final_df_2011)
 str(df)
 table(is.na(df$Salary))
-df[is.na(df$Salary)==T,]
+median(df$Salary)

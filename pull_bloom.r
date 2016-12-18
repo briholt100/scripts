ca0b4412b4879fc35fb8a367ca5fac501f6fced6
Brian Holt <brian.c.holt@gmail.com>
2016-12-17 22:17:10 -0800

    colnames for _df

--- pull_bloom.r
+++ pull_bloom.r
@@ -404,12 +404,16 @@ ac<-read.csv(file="./agency_code.csv")
 ###### these should happen after all of 2003-2011 are merged but before merging with colleges_longForm
 final_df <- merge(college_df,ac, by.x="Institution", by.y="Institute", all.x=TRUE)  #this gets the correct names of agencies
 #final_df<- (final_df[,c(10,11,3:4,9,2,5:8)])
-final_df %>% select(10,11,3:4,9,2,5:8) %>%
+final_df %>% select(Code,Agency,Employee,Job.Title,job.cat,year,Salary,et:percent_ft) %>%
   head()
-final_df<-final_df %>% select(10,11,3:4,9,2,5:8) %>% rename(Agency_Title=Agency,employee_name=Employee,job_title=Job.Title) #note this drops et, mp, etc
+final_df<-final_df %>% select(Code,Agency,Employee,Job.Title,job.cat,year,Salary,et:percent_ft) %>%
+  rename(Agency_Title=Agency,employee_name=Employee,job_title=Job.Title)
+
 head(final_df)
+
 final_df$job.cat<-ifelse(grepl("facul|FTF|fac sub|pt-fac|pro-rata",final_df$job_title,ignore.case=T),"Faculty","Non-fac")
 
+# "Code","Agency_Title","employee_name","job_title","year","Salary","job.cat"
 
 #final_df<-rbind(final_df,colleges_longForm)   ##########this rbinds salary and finaldf2011
 write.csv(final_df, file = "./final_df.csv")

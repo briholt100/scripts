2148eb2be91728f98a1fc37ed0860c2bc340e778
briholt100 <brian.c.holt@gmail.com>
2017-09-27 14:28:40 -0700

    python 3 code that crawls twitter friends makes sqlite

---
+++ hidden.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 27 14:11:10 2017
+
+@author: bholt
+"""
+
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "vKGQwYj80Kvj0iMdxEvJR89Cu",
+            "consumer_secret": "JdQBLceXGJTS0eSoOeKWlUmIOWR4bQDfYx0muopEOjjHnjDPTJ",
+            "token_key": "23893139-VGiZBIUrHsGEBPbmh9zfuXEmyopSMboUGkd6DwhFV",
+            "token_secret": "IXUV55jTUntb9uEzLe4zJTnSFLkD1aEepeLAuMmzqb0FK"}
+
+# Code: ./my data sources/script /hidden.py

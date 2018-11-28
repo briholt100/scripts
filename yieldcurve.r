99a759440fa552b0e52f975d8ed8ae0f2954a3b7
Holt <Brian.Holt@seattlecolleges.edu>
2018-11-28 15:01:16 -0800

    added vert line for 2016 election

--- yieldcurve.r
+++ yieldcurve.r
@@ -18,11 +18,13 @@ p+labs(y='rates')+scale_colour_manual(values=c("red","green","blue","black"))+
   geom_point(aes(y=yc$df$BC_1MONTH),col='black')+
   geom_smooth(data=yc$df,aes(dates,BC_30YEAR))
 
-plot(yc$df$BC_1MONTH~yc$df$dates,type='l',ylim=c(0,6))
-lines(yc$df$BC_3MONTH~yc$df$dates,type='l',col='red')
-lines(yc$df$BC_10YEAR~yc$df$dates,type='l',col='blue')
-lines(yc$df$BC_30YEAR~yc$df$dates,type='l',col='green')
+df1<-yc$df %>% tail(700)
+plot(df1$BC_1MONTH~df1$dates,type='l',ylim=c(0,6))
+lines(df1$BC_3MONTH~df1$dates,type='l',col='red')
+lines(df1$BC_10YEAR~df1$dates,type='l',col='blue')
+lines(df1$BC_30YEAR~df1$dates,type='l',col='green')
+#abline(v=2016-11-6,col='red')
 
 
-yc$df %>% gather(bond,rate,-dates) %>% filter(bond == "BC_30YEAR" | bond == "BC_20YEAR"| bond == "BC_3MONTH"
-) %>% ggplot(aes(x=dates,y=rate,color=bond)) + geom_line()
+yc$df %>% tail(700) %>% gather(bond,rate,-dates) %>% filter(bond == "BC_30YEAR" | bond == "BC_20YEAR"| bond == "BC_3MONTH"
+) %>% ggplot(aes(x=dates,y=rate,color=bond)) + geom_line() +geom_vline(xintercept = as.numeric(as.Date("2016-11-06")))

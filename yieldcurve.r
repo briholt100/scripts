09793646ad57e5765b2d4425e86aa7ea08aab144
Holt <Brian.Holt@seattlecolleges.edu>
2018-12-06 15:20:31 -0800

    looking at 2004 april

--- yieldcurve.r
+++ yieldcurve.r
@@ -18,7 +18,9 @@ p+labs(y='rates')+scale_colour_manual(values=c("red","green","blue","black"))+
   geom_point(aes(y=yc$df$BC_1MONTH),col='black')+
   geom_smooth(data=yc$df,aes(dates,BC_30YEAR))+
   geom_vline(xintercept = as.numeric(as.Date("2016-11-06")),linetype='dashed')+
-  geom_hline(yintercept = .81)
+  geom_hline(yintercept = .81)+
+  geom_vline(xintercept = as.numeric(as.Date("2003-06-19")),linetype='dashed')
+  
 
 df1<-yc$df %>% tail(700)
 plot(df1$BC_1MONTH~df1$dates,type='l',ylim=c(0,6))
@@ -28,11 +30,16 @@ lines(df1$BC_30YEAR~df1$dates,type='l',col='green')
 abline(v=as.numeric(as.Date("2016-11-06")),col='black',lty=3)
 
 
+
 yc$df %>% 
   #tail(700) %>% 
-  filter(BC_3MONTH > .5 & BC_3MONTH< .83) %>% 
+  filter(dates > as.numeric(as.Date("2003-06-01")) & dates < as.numeric(as.Date("2010-06-01"))) %>% 
+  #filter(BC_3MONTH > .5 & BC_3MONTH< .83) %>% 
   gather(bond,rate,-dates) %>% 
   filter(bond == "BC_30YEAR" | bond == "BC_20YEAR"| bond == "BC_3MONTH") %>% 
   ggplot(aes(x=dates,y=rate,color=bond)) + 
   geom_line() +
   geom_vline(xintercept = as.numeric(as.Date("2016-11-06")),linetype='dashed')
+
+yc$df %>% select(BC_3MONTH,dates) %>% filter(dates > as.numeric(as.Date("2004-03-01")) & dates < as.numeric(as.Date("2004-06-01"))) %>% ggplot(aes(x=dates,y=BC_3MONTH))+geom_line()
+yc$df %>%  filter(dates > as.numeric(as.Date("2003-03-01")) & dates < as.numeric(as.Date("2008-06-01"))) %>%   gather(bond,rate,-dates) %>% ggplot(aes(x=dates,y=rate,color=bond))+geom_line()+geom_vline(xintercept = as.numeric(as.Date("2004-04-01")),linetype='dashed')

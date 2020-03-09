4c1f2af86abe63e8c8c835856503fa2b02331feb
brian <brian.c.holt@gmail.com>
2020-03-08 23:12:10 -0700

    changed source for unemployment to html

--- yieldcurve.r
+++ yieldcurve.r
@@ -12,18 +12,18 @@ names.yc<-names(yc)
 
 
 names(yc)<-gsub("([0-9]{1,2}) ([A-Z]{2})","\\2\\1",names.yc)
-today<-yc %>% 
-  arrange(desc(Date)) %>% 
-  head(10) %>% 
-  gather(bond,rate, -Date) %>% 
-  arrange(desc(Date)) %>% 
+today<-yc %>%
+  arrange(desc(Date)) %>%
+  head(10) %>%
+  gather(bond,rate, -Date) %>%
+  arrange(desc(Date)) %>%
   head(12) %>%
   arrange(desc(rate))
 today
-yc %>% 
-  arrange(desc(Date)) %>% 
-  head(5) %>% 
-  gather(bond,rate, -Date) %>% 
+yc %>%
+  arrange(desc(Date)) %>%
+  head(5) %>%
+  gather(bond,rate, -Date) %>%
   filter(#bond == '1 MO' |bond == '2 MO' |bond == '3 MO' |
     bond == '6 MO' |
       bond == '3 YR' |
@@ -31,28 +31,28 @@ yc %>%
       bond == '5 YR'|
       bond == '1 YR'|
       bond == '7 YR')  %>% with(plot(rate~Date,data = . ))
-  
-yc %>% 
-  arrange(desc(Date)) %>% 
-  head(105) %>% 
-  gather(bond,rate, -Date) %>% 
-  group_by(bond,Date) %>% 
-  filter(Date>"2020-02-01") %>% 
-  filter(grepl('YR',bond)) %>% 
-  filter(nchar(bond)<4) %>% 
-  ggplot(aes(x=Date,y=rate,color=bond)) + 
+
+yc %>%
+  arrange(desc(Date)) %>%
+  head(105) %>%
+  gather(bond,rate, -Date) %>%
+  group_by(bond,Date) %>%
+  filter(Date>"2020-02-01") %>%
+  filter(grepl('YR',bond)) %>%
+  filter(nchar(bond)<4|bond=='YR30') %>%
+  ggplot(aes(x=Date,y=rate,color=bond)) +
   geom_line() +
   geom_point(data=today, aes(y = today$rate))+geom_text(data=today,aes(label=bond),hjust=0, vjust=-.8)
-  
 
 
 
-yc %>% 
-  arrange(desc(Date)) %>% 
-  head(10) %>% 
-  mutate(one=YR7-YR1,six=YR7-MO6,two=YR7-YR2) %>% 
-  select(one,six,two,Date) %>% 
-  gather(key=bond,value=rate,-Date) %>% group_by(bond, Date) %>% 
+
+yc %>%
+  arrange(desc(Date)) %>%
+  head(10) %>%
+  mutate(one=YR7-YR1,six=YR7-MO6,two=YR7-YR2) %>%
+  select(one,six,two,Date) %>%
+  gather(key=bond,value=rate,-Date) %>% group_by(bond, Date) %>%
   ggplot(aes(x=Date,y=rate,color=bond))+geom_line()
 
 
@@ -73,7 +73,7 @@ p+labs(y='rates')+scale_colour_manual(values=c("red","green","blue","black"))+
   geom_vline(xintercept = as.numeric(as.Date("2016-11-06")),linetype='dashed')+
   geom_hline(yintercept = .81)+
   geom_vline(xintercept = as.numeric(as.Date("2003-06-19")),linetype='dashed')
-  
+
 
 df1<-yc$df %>% tail(700)
 plot(df1$BC_1MONTH~df1$dates,type='l',ylim=c(0,6))
@@ -84,13 +84,13 @@ abline(v=as.numeric(as.Date("2016-11-06")),col='black',lty=3)
 
 
 
-yc$df %>% 
-  #tail(700) %>% 
-  filter(dates > as.numeric(as.Date("2003-06-01")) & dates < as.numeric(as.Date("2010-06-01"))) %>% 
-  #filter(BC_3MONTH > .5 & BC_3MONTH< .83) %>% 
-  gather(bond,rate,-dates) %>% 
-  filter(bond == "BC_30YEAR" | bond == "BC_20YEAR"| bond == "BC_3MONTH") %>% 
-  ggplot(aes(x=dates,y=rate,color=bond)) + 
+yc$df %>%
+  #tail(700) %>%
+  filter(dates > as.numeric(as.Date("2003-06-01")) & dates < as.numeric(as.Date("2010-06-01"))) %>%
+  #filter(BC_3MONTH > .5 & BC_3MONTH< .83) %>%
+  gather(bond,rate,-dates) %>%
+  filter(bond == "BC_30YEAR" | bond == "BC_20YEAR"| bond == "BC_3MONTH") %>%
+  ggplot(aes(x=dates,y=rate,color=bond)) +
   geom_line() +
   geom_vline(xintercept = as.numeric(as.Date("2016-11-06")),linetype='dashed')
 
